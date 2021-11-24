668b272abf62aec6aa6a92344450bf26d46b41d1
Yaros <Yaroslavhik@yandex.ua>
2021-11-24 16:18:09 +0100

    installed HtmlWebpackPlugin for starting of webpack but with error, it is not show the h1 from index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,23 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: {
-        app: './src/index.js'
+        app: './src/index.js',
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+        }),
+    ],
     output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, '../dist'),
-        publicPath: '/dist'
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        publicPath: "/"
     },
     devServer: {
-        client : {
-            overlay: true
-        }
+        static: './dist'
     }
-}
+};
